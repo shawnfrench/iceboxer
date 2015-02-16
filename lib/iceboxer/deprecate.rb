f26cd91d0eb4c9dcad047858c772d1357526e489
Tom Burns <tom.burns@jadedpixel.com>
2015-02-16 16:20:10 +0000

    also ping assignee

--- lib/iceboxer/deprecate.rb
+++ lib/iceboxer/deprecate.rb
@@ -8,23 +8,29 @@ module Iceboxer
       @repo = repo
     end
 
+    def user_string(issue)
+      ret = [issue.user.login]
+      ret.push(issue.assignee.login) if issue.assignee.present?
+      ret.map { |u| "@#{u}" }.join(" ")
+    end
+
     def perform
       issues = Octokit.search_issues("repo:#{@repo} is:open label:Deprecation")
       issues.items.each do |issue|
         begin
           date = issue.title.to_date
           if date < Date.today
-            Octokit.add_comment(@repo, issue.number, deprecate(issue.user.login, (Date.today - date).to_i))
+            Octokit.add_comment(@repo, issue.number, deprecate(user_string(issue), (Date.today - date).to_i))
           end
         rescue ArgumentError
-          Octokit.add_comment(@repo, issue.number, add_date_message(issue.user.login))
+          Octokit.add_comment(@repo, issue.number, add_date_message(user_string(issue)))
         end
       end
     end
 
-    def add_date_message(user)
+    def add_date_message(users)
       <<-MSG
-@#{user},
+#{users},
 
 Issues with the 'Deprecation' label need to have a deprecation date 
 in the issue title, in format YYYY-MM-DD.
@@ -35,9 +41,9 @@ Please add a date to the title, or remove the 'Deprecation' label.
       MSG
     end
 
-    def deprecate(user, days)
+    def deprecate(users, days)
       message = <<-MSG
-Ping @#{user}
+Ping #{users}
 
 This deprecation notice expired #{days} #{"day".pluralize(days)} ago.
 
