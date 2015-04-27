ddd0d00cdab282e097e9e812dc3fd99b02b8057a
Shawn French <shawn.french@jadedpixel.com>
2015-04-27 18:28:01 +0000

    Support multiple repos

--- lib/iceboxer/task.rb
+++ lib/iceboxer/task.rb
@@ -13,15 +13,17 @@ module Iceboxer
       raise "Set GITHUB_API_TOKEN with a token with repo access"
     end
 
-    unless ENV['ICEBOXER_REPO'].present?
-      raise "Set ICEBOXER_REPO to repo like 'org/reponame'"
+    unless ENV['ICEBOXER_REPOS'].present?
+      raise "Set ICEBOXER_REPOS to repo(s) like 'org/repo1, org/repo2'"
     end
 
     Octokit.access_token = ENV['GITHUB_API_TOKEN']
-    repository = ENV["ICEBOXER_REPO"]
+    repositories = ENV["ICEBOXER_REPOS"].split(',').map(&:strip)
 
     @@operations.each do |op|
-      op.new(repository).perform
+      repositories.each do |repository|
+        op.new(repository).perform
+      end
     end
   end
 end
